355e07bc6788908696c272afef90768d5d1a787b
mos <mos.cheele+github@gmail.com>
2023-06-12 12:16:23 +0200

    Add some runtime information

--- src/main/java/mos/quarkus/play/ExampleService.java
+++ src/main/java/mos/quarkus/play/ExampleService.java
@@ -1,11 +1,27 @@
 package mos.quarkus.play;
 
+import io.quarkus.runtime.QuarkusApplication;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class ExampleService {
 
+	@ConfigProperty(name = "quarkus.application.version")
+	String applicationVersion;
+
+	@ConfigProperty(name = "quarkus.application.name")
+	String applicationName;
+
+	@ConfigProperty(name = "platform.quarkus.native.builder-image")
+	String nativeBuildImage;
+
+
 	public String generateText() {
-		return "Hello World! Quarkus is coming...";
+		return "Hello World! Quarkus is running. Environment: \n\n" +
+				"Application name: " + applicationName + "\n" +
+				"Application version: " + applicationVersion + "\n" +
+				"NativeBuildImage: " + nativeBuildImage;
 	}
 }
