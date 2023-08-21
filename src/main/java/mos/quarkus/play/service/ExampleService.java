86a565a56ebd455dfe0de2edb56c7704729c92ec
mos <mos.cheele+github@gmail.com>
2023-08-21 17:22:40 +0200

    Show Quarkus-Version number and update Quarkus version.

--- src/main/java/mos/quarkus/play/service/ExampleService.java
+++ src/main/java/mos/quarkus/play/service/ExampleService.java
@@ -16,9 +16,13 @@ public class ExampleService {
 	@ConfigProperty(name = "platform.quarkus.native.builder-image")
 	String nativeBuildImage;
 
+	@ConfigProperty(name = "quarkus.framework.version")
+	String quarkusFrameworkVersion;
+
 
 	public String generateText() {
 		return "Hello World! Quarkus is running. \n\n" +
+				"Quarkus Version: " + quarkusFrameworkVersion + "\n" +
 				"Profile: " + ConfigUtils.getProfiles() + "\n" +
 				"Application name: " + applicationName + "\n" +
 				"Application version: " + applicationVersion + "\n" +
