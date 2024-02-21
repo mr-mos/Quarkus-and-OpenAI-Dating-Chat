7b8adc724f090797a32e397cf1c79b33e0670e91
mos <mos.cheele+github@gmail.com>
2024-02-21 17:24:56 +0100

    Qute:  Access to enum values

--- src/main/java/mos/quarkus/play/model/Globals.java
+++ src/main/java/mos/quarkus/play/model/Globals.java
@@ -1,17 +1,24 @@
 package mos.quarkus.play.model;
 
 import io.quarkus.arc.Arc;
+import io.quarkus.qute.TemplateExtension;
 import io.quarkus.qute.TemplateGlobal;
+import mos.quarkus.play.defs.ChatGoal;
 import mos.quarkus.play.service.NavigationService;
 
 import java.util.Map;
 
-@TemplateGlobal
 public class Globals {
 
 
+	@TemplateGlobal
 	static Map<String, String> navigation() {
 		return Arc.container().instance(NavigationService.class).get().getNavigation();
+	}
+
+	@TemplateExtension(namespace = "ChatGoal", matchName = "*")
+	static ChatGoal enumValue(String value) {
+		return ChatGoal.valueOf(value);
 	}
 
 }
